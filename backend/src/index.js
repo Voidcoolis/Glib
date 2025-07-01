661933b0a8b5817a91ebd97fe9b7a785b1c3bf36
Bonjov Hima <bonjovhima@hotmail.de>
2025-07-01 21:02:28 +0200

    created user & message model for database setup & added the mongoDb uri to the env file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import { connectDB } from "./lib/db.js";
 
+dotenv.config(); // Load environment variables from .env file
 const app = express();
+
+const PORT = process.env.PORT;
 
 // Route for authentification
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB(); // connects to MongoDB database
 })
